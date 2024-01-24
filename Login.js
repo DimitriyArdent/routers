c8541a7279c59e0267a6c9f028054ae0df11a553
DimitriyArdent <109689205+DimitriyArdent@users.noreply.github.com>
2024-01-24 12:58:27 +0200

    1

---
+++ Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+            <label>name:</label>
+            <input></input>
+            <label>password:</label>
+            <input></input>
+    </div>
+  )
+}
+
+export default Login
